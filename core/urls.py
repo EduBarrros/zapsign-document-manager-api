4408f7db724a176ab243a1d2fcc11b4ab6138cf4
EduBarrros <eduardodeoliveirabarros@gmail.com>
2026-06-07 13:25:55 -0300

    chore: fix schema endpoint and enhance Swagger documentation across multiple views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.renderers import JSONRenderer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +9,6 @@ urlpatterns = [
     path('api/v1/', include('apps.companies.urls')),
     path('api/v1/', include('apps.documents.urls')),
     path('api/v1/', include('apps.signers.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(renderer_classes=[JSONRenderer]), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
